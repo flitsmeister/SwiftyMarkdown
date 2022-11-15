6330e3e4067bb9d076d4b7bcd11af9fd2368a543
Sander <sandervantulden@gmail.com>
2022-11-15 15:27:01 +0100

    Heavy bold to prevent semi-bold

--- Sources/SwiftyMarkdown/SwiftyMarkdown+iOS.swift
+++ Sources/SwiftyMarkdown/SwiftyMarkdown+iOS.swift
@@ -149,7 +149,7 @@ extension SwiftyMarkdown {
         } else if globalBold, !ignoresDynamicSize, let boldDescriptor = font.fontDescriptor.withSymbolicTraits(.traitBold) {
             font = UIFont(descriptor: boldDescriptor, size: fontSize ?? 0)
         } else if globalBold, ignoresDynamicSize {
-            font = UIFont.boldSystemFont(ofSize: fontSize ?? 0)
+            font = UIFont.systemFont(ofSize: fontSize ?? 0, weight: .heavy)
         } else if globalItalic, let italicDescriptor = font.fontDescriptor.withSymbolicTraits(.traitItalic) {
             font = UIFont(descriptor: italicDescriptor, size: fontSize ?? 0)
         }
